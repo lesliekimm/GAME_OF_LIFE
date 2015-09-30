865eb63138ab4925b6e2b8d4c75d3b08bcaf30f9
leslie kimm <leslie.kimm@gmail.com>
2015-09-29 22:57:38 -0700

    Defined setUpGrid in Grid

--- GameOfLife.spritebuilder/Source/Grid.swift
+++ GameOfLife.spritebuilder/Source/Grid.swift
@@ -1,5 +1,40 @@
 import UIKit
 
-class Grid: CCSprite {
+let GridRows = 8
+let GridColumns = 10
 
+class Grid: CCSprite {
+    var totalAlive = 0
+    var generation = 0
+    
+    var cellWidth: CGFloat = 0
+    var cellHeight: CGFloat = 0
+    var gridArray: [[Creature]]!
+    
+    override func onEnter() {
+        super.onEnter()
+        
+        setUpGrid()
+        
+        userInteractionEnabled = true
+    }
+    
+    func setUpGrid() {
+        cellWidth = contentSize.width / CGFloat(GridColumns)
+        cellHeight = contentSize.height / CGFloat(GridRows)
+        
+        gridArray = []
+        
+        for row in 0..<GridRows {
+            gridArray.append([])
+            
+            for column in 0..<GridColumns {
+                var creature = Creature()
+                creature.position = CGPoint(x: cellWidth * CGFloat(column), y: cellHeight * CGFloat(row))
+                addChild(creature)
+                gridArray[row].append(creature)
+                creature.isAlive = false
+            }
+        }
+    }
 }
