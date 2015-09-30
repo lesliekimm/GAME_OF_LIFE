ffd40a0d079323013955a6ca2a142989a8a3e16a
leslie kimm <leslie.kimm@gmail.com>
2015-09-29 22:44:12 -0700

    Define init function for Creature

--- GameOfLife.spritebuilder/Source/Creature.swift
+++ GameOfLife.spritebuilder/Source/Creature.swift
@@ -1,5 +1,17 @@
 import UIKit
 
 class Creature: CCSprite {
-
+    // proerty observer - when we set it to alive, it's visible and not when it's dead
+    var isAlive = false {
+        didSet {
+            self.visible = isAlive
+        }
+    }
+    var livingNeighborsCount = 0
+    
+    // convenience tells compiler that a secod initializer will be called
+    convenience override init() {
+        self.init(imageNamed: "GameOfLifeAssets/Assets/bubble.png") // set image to bubble.png
+        anchorPoint = CGPoint(x: 0, y: 0)
+    }
 }
